901364df5d68632b16b0f5e53b070b54bd0d7ca1
danielaush <danielekpace@gmail.com>
2025-06-05 17:38:19 +0200

    debug

--- circuit-tracer/src/safety_circuit_discovery.py
+++ circuit-tracer/src/safety_circuit_discovery.py
@@ -118,7 +118,7 @@ class SafetyCircuitAnalyzer:
         
         contrastive_features = {}
         
-        safe_categories = [c for c in self.feature_stats.keys() if c not in safe_categories]
+        unsafe_categories = [c for c in self.feature_stats.keys() if c not in safe_categories]
         safe_features = set([self.feature_stats[safe_category].keys() for safe_category in safe_categories])
         
         for unsafe_cat in unsafe_categories:
